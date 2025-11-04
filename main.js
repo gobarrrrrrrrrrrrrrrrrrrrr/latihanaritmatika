2b38559bcd0220ad97350ca88f466e0a9036b9dc
gobarrrrrrrrrrrrrrrrrrrrr <faridmieayamanjai@gmail.com>
2025-11-04 11:18:51 +0700

    aritmatika

--- main.js
+++ main.js
@@ -12,11 +12,19 @@ function hitung() {
 let subtotal = harga * jumlah 
 
 //langkah 2: menghitung diskon
-// langlah 3: menghitung pajak
+let potongan = subtotal * (diskon/ 100)
+
+// langlah 3: menghitung pajak,misal 11%
+let pajak = subtotal * (11/100)
+
 // lanhkah 4: hitung total
+let total = subtotal - potongan + pajak
 
 // langkah 5: menampilkan hasil
 document.getElementById("hasil").innerHTML = `
 Subtotal: Rp ${subtotal} <br>
+Diskon (${diskon}%): -Rp ${potongan } <br>
+pajak (11%) : +RP ${pajak} <br>
+total bayar : Rp ${total}
 `
 }
